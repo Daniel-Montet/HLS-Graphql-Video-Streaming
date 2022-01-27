8c69f9474e8a6e127d7336180a571795a2342bc8
Daniel Montet <dmontetproff@gmail.com>
2022-01-27 15:22:34 +0300

    inittialized db using typeorm

---
+++ server/ormconfig.ts
@@ -0,0 +1,12 @@
+module.exports = {
+	"name": "default",
+	"type": "postgres",
+	"host": "localhost",
+	"port": 5432,
+	"username": "hlsvideo",
+	"password": "hlsvideo",
+	"database": "hlsvideo",
+	"uuidExtension": "pgcrypto",
+	"entities": ["src/*/entity/*.ts"],
+	"synchronize": true
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,28 @@
+import { createConnection, Connection, getConnection } from "typeorm";
 import express from "express";
 import 'dotenv/config'
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.get("/", (_, res) => {
-	res.send("Server up")
-})
+(async () => {
+	// init db connection
+	try {
+		await createConnection()
+	} catch (err) {
+		console.log("db error")
+		console.log(err)
+	}
 
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`)
-})
+	const db: Connection = getConnection();
+	console.log("db connected: ", db.isConnected)
+
+
+	app.get("/", (_, res) => {
+		res.send("Server up")
+	})
+
+	app.listen(port, () => {
+		console.log(`Server running on port ${port}`)
+	})
+})()
